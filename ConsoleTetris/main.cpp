0492d827ad7453f5419532042f162d6fdcb81b97
Jaeho Kim <bestway27@gmail.com>
2022-11-15 00:58:51 +0900

    커밋

--- ConsoleTetris/main.cpp
+++ ConsoleTetris/main.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <conio.h>
-#include <algorithm>
 #include <time.h>
 
 struct Pos
@@ -19,7 +18,7 @@ struct Pos
 } currentPos;
 
 const int WALL_START_X = 50;
-const int WALL_START_Y = 4;
+const int WALL_START_Y = 6;
 
 const int WALL_LENGTH_X = 2;
 const int WALL_LENGTH_Y = 1;
@@ -32,11 +31,13 @@ const int MAX_COL = 10;
 enum Key { UP = 72, LEFT = 75, RIGHT = 77, DOWN = 80, P = 112, Q = 113, SPACE = 32 };
 enum Speed { SPEED1 = 1000, SPEED2 = 500, MAX_SPEED = 10 };
 enum Object {EMPTY = 0, WALL = -1};
-enum Color {BLUE = 1, GREEN = 2, CYAN = 3, RED = 4, MAGENTA = 5, GOLD = 6, DEFAULT = 7, SKYBLUE = 11, YELLOW = 14, WHITE = 15};
+enum Color {BLUE = 1, GREEN = 2, CYAN = 3, RED = 4, MAGENTA = 5, GOLD = 6, DEFAULT = 7,
+			LIGHTGREEN = 10, SKYBLUE = 11, LIGHTRED = 12, YELLOW = 14, WHITE = 15};
 
 ULONGLONG prev_time;
 
 bool gameIsRunning;
+bool comboAvailable;
 
 int map[MAX_ROW+1][MAX_COL] = { EMPTY };
 
@@ -52,18 +53,28 @@ int autoDropSpeed;
 
 int currentBlock[MAX_BLOCK_LENGTH][MAX_BLOCK_LENGTH] = { 0 };
 int currentBlockLength;
+int currentBlockColor;
+int nextBlock[MAX_BLOCK_LENGTH][MAX_BLOCK_LENGTH] = { 0 };
+int nextBlockLength;
+int nextBlockColor;
 
-void gotoxy(short x, short y);
-void gotoxyInside(short x, short y);
+int lineClearAtOnce;
+int currentCombo;
+int maxCombo;
+int totalScore;
 
-void createNewBlock();
+void gotoXY(short x, short y);
+void gotoInsideXY(short x, short y);
+
+void createNextBlock();
+void updateCurrentBlock();
 void drawBlock();
 void eraseBlock();
 bool moveBlock(int r, int c);
-void copyBlock(int* src, int* dest);
+void copyBlock(int src[][MAX_BLOCK_LENGTH], int dest[][MAX_BLOCK_LENGTH]);
 
 void setTextColor(int color);
-void setBlockColor(int color);
+void setNextBlockColor(int color);
 
 void moveDown();
 void moveLeft();
@@ -83,6 +94,7 @@ void hideCursor();
 void initGame();
 void gameOver();
 void drawBackground();
+void updateScore();
 void keyProcess();
 void run();
 
@@ -92,28 +104,50 @@ int main()
 }
 
 
-void gotoxy(short x, short y) {
+void gotoXY(short x, short y) {
 	x += WALL_START_X;
 	y += WALL_START_Y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { x, y });
 }
-void gotoxyInside(short x, short y) {
-	gotoxy(x + WALL_LENGTH_X, y + WALL_LENGTH_Y);
+void gotoInsideXY(short x, short y) {
+	gotoXY(x + WALL_LENGTH_X, y + WALL_LENGTH_Y);
 }
 
-void createNewBlock()
+void createNextBlock()
 {
 	int v = rand() % BLOCK_TYPE_COUNT;
 	switch (v)
 	{
-	case 0:  copyBlock(*block_T, *currentBlock); currentBlockLength = 3; setBlockColor(MAGENTA);  break;
-	case 1:  copyBlock(*block_I, *currentBlock); currentBlockLength = 4; setBlockColor(CYAN);     break;
-	case 2:  copyBlock(*block_O, *currentBlock); currentBlockLength = 2; setBlockColor(YELLOW);   break;
-	case 3:  copyBlock(*block_J, *currentBlock); currentBlockLength = 3; setBlockColor(BLUE);     break;
-	case 4:  copyBlock(*block_L, *currentBlock); currentBlockLength = 3; setBlockColor(GOLD);     break;
-	case 5:  copyBlock(*block_S, *currentBlock); currentBlockLength = 3; setBlockColor(GREEN);    break;
-	default: copyBlock(*block_Z, *currentBlock); currentBlockLength = 3; setBlockColor(RED);      break;
+	case 0:  copyBlock(block_T, nextBlock); nextBlockLength = 3; setNextBlockColor(MAGENTA);  break;
+	case 1:  copyBlock(block_I, nextBlock); nextBlockLength = 4; setNextBlockColor(CYAN);     break;
+	case 2:  copyBlock(block_O, nextBlock); nextBlockLength = 2; setNextBlockColor(YELLOW);   break;
+	case 3:  copyBlock(block_J, nextBlock); nextBlockLength = 3; setNextBlockColor(BLUE);     break;
+	case 4:  copyBlock(block_L, nextBlock); nextBlockLength = 3; setNextBlockColor(GOLD);     break;
+	case 5:  copyBlock(block_S, nextBlock); nextBlockLength = 3; setNextBlockColor(GREEN);    break;
+	default: copyBlock(block_Z, nextBlock); nextBlockLength = 3; setNextBlockColor(RED);      break;
 	}
+
+	setTextColor(nextBlockColor);
+	for (int i = 0; i < MAX_BLOCK_LENGTH; i++)
+	{
+		for (int j = 0; j < MAX_BLOCK_LENGTH; j++)
+		{
+			gotoXY((MAX_COL + 4 + j) * 2, i + 4);
+			if (nextBlock[i][j] != EMPTY)
+				printf("■");
+			else
+				printf("  ");
+		}
+	}
+	setTextColor(currentBlockColor);
+}
+
+void updateCurrentBlock()
+{
+	copyBlock(nextBlock, currentBlock);
+	currentBlockLength = nextBlockLength;
+	currentBlockColor = nextBlockColor;
+	setTextColor(currentBlockColor);
 	currentPos = Pos(0, MAX_COL / 2 - 1);
 	drawBlock();
 }
@@ -125,7 +159,7 @@ void drawBlock() {
 		{
 			if (currentBlock[i][j] != EMPTY)
 			{
-				gotoxyInside((currentPos.col + j) * 2, currentPos.row + i);
+				gotoInsideXY((currentPos.col + j) * 2, currentPos.row + i);
 				printf("■");
 			}
 		}
@@ -138,7 +172,7 @@ void eraseBlock() {
 		{
 			if (currentBlock[i][j] != EMPTY)
 			{
-				gotoxyInside((currentPos.col + j) * 2, currentPos.row + i);
+				gotoInsideXY((currentPos.col + j) * 2, currentPos.row + i);
 				printf("  ");
 			}
 		}
@@ -158,9 +192,15 @@ bool moveBlock(int r, int c)
 	return true;
 }
 
-void copyBlock(int* src, int* dest)
+void copyBlock(int src[][MAX_BLOCK_LENGTH], int dest[][MAX_BLOCK_LENGTH])
 {
-	std::copy(src, src + (MAX_BLOCK_LENGTH * MAX_BLOCK_LENGTH), dest);
+	for (int i = 0; i < MAX_BLOCK_LENGTH; i++)
+	{
+		for (int j = 0; j < MAX_BLOCK_LENGTH; j++)
+		{
+			dest[i][j] = src[i][j];
+		}
+	}
 }
 
 void setTextColor(int color)
@@ -168,21 +208,22 @@ void setTextColor(int color)
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
-void setBlockColor(int color)
+void setNextBlockColor(int color)
 {
 	for (int i = 0; i < MAX_BLOCK_LENGTH; i++)
 	{
 		for (int j = 0; j < MAX_BLOCK_LENGTH; j++)
 		{
-			currentBlock[i][j] = currentBlock[i][j] * color;
+			nextBlock[i][j] = nextBlock[i][j] * color;
 		}
 	}
-	setTextColor(color);
+	nextBlockColor = color;
 }
 
 void moveDown()
 {
 	prev_time = GetTickCount64(); // 블럭 하강 시, 프레임 카운트 무조건 초기화
+	lineClearAtOnce = 0;
 
 	bool blockReachedTheEnd = !moveBlock(1, 0); // 블럭을 한 칸 하강한다.
 
@@ -205,12 +246,34 @@ void moveDown()
 						map[x][y] = map[x - 1][y];
 					}
 				}
-				redrawMap();
+				lineClearAtOnce++;
 			}
 		}
 
-		autoDropSpeed = SPEED2;
-		createNewBlock();
+		if (lineClearAtOnce > 0)
+		{
+			if (comboAvailable == true)
+			{
+				currentCombo++;
+				if (currentCombo > maxCombo)
+					maxCombo = currentCombo;
+			}
+			else
+			{
+				comboAvailable = true;
+			}
+		}
+		else
+		{
+			comboAvailable = false;
+			currentCombo = 0;
+		}
+		redrawMap();
+		updateScore();
+
+		autoDropSpeed = SPEED1;
+		updateCurrentBlock();
+		createNextBlock();
 	}
 }
 
@@ -269,7 +332,7 @@ void rotateBlock()
 		return;
 
 	int tempBlock[MAX_BLOCK_LENGTH][MAX_BLOCK_LENGTH] = { 0 };
-	copyBlock(*currentBlock, *tempBlock);
+	copyBlock(currentBlock, tempBlock);
 
 	eraseBlock();
 	for (int i = 0; i < currentBlockLength; i++)
@@ -319,7 +382,7 @@ void redrawMap()
 	{
 		for (int j = 0; j < MAX_COL; j++)
 		{
-			gotoxyInside(j * 2, i);
+			gotoInsideXY(j * 2, i);
 			setTextColor(map[i][j]);
 
 			if (map[i][j] == EMPTY)
@@ -347,11 +410,20 @@ void initGame()
 {
 	srand((unsigned int)time(0));
 	prev_time = GetTickCount64();
-	autoDropSpeed = SPEED2;
+	autoDropSpeed = SPEED1;
+
+	lineClearAtOnce = 0;
+	currentCombo = 0;
+	maxCombo = 0;
+	totalScore = 0;
+	comboAvailable = false;
 
 	drawBackground();
+	updateScore();
 	hideCursor();
-	createNewBlock();
+	createNextBlock();
+	updateCurrentBlock();
+	createNextBlock();
 	gameIsRunning = true;
 
 	for (int i = 0; i < MAX_COL; i++)
@@ -361,7 +433,7 @@ void initGame()
 void gameOver()
 {
 	gameIsRunning = false;
-	gotoxy(MAX_COL - 3, MAX_ROW / 2);
+	gotoXY(MAX_COL - 3, MAX_ROW / 2);
 	setTextColor(WHITE);
 	printf(" Game Over"); // G가 지워지는 버그 때문에 앞에 한 칸 공백 추가
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });
@@ -369,59 +441,144 @@ void gameOver()
 
 void drawBackground()
 {
+	setTextColor(LIGHTRED);
+	gotoXY(MAX_COL - 4, -2);
+	printf("T E T R I S");
+
 	setTextColor(WHITE);
 
 	// 벽 그리기 (가로)
 	for (int i = 0; i < MAX_COL + 2; i++)
 	{
-		gotoxy((short)(i * 2), 0);
+		gotoXY((short)(i * 2), 0);
 		printf("▨");
-		gotoxy((short)(i * 2), MAX_ROW + 1);
+		gotoXY((short)(i * 2), MAX_ROW + 1);
 		printf("▨");
 	}
 
 	// 벽 그리기 (세로)
 	for (int i = 0; i < MAX_ROW + 1; i++)
 	{
-		gotoxy(0, (short)i);
+		gotoXY(0, (short)i);
 		printf("▨\n");
-		gotoxy((MAX_COL * 2) + 2, (short)i);
+		gotoXY((MAX_COL * 2) + 2, (short)i);
 		printf("▨\n");
 	}
 
-	// 현황판 벽 그리기 (가로)
+	// 왼쪽 현황판 벽 그리기 (가로)
 	for (int i = 0; i < 9; i++)
 	{
-		gotoxy((short)(i * 2 - 18), 0);
+		gotoXY((short)(i * 2 - 16), 0);
 		printf("▧");
-		gotoxy((short)(i * 2 - 18), 7);
+		gotoXY((short)(i * 2 - 16), 7);
 		printf("▧");
-		gotoxy((short)(i * 2 - 18), 13);
-		printf("▧");
-		gotoxy((short)(i * 2 - 18), 21);
+		gotoXY((short)(i * 2 - 16), 21);
 		printf("▧");
 	}
 
-	// 현황판 벽 그리기 (세로)
+	// 왼쪽 현황판 벽 그리기 (세로)
 	for (int i = 0; i < 21; i++)
 	{
-		gotoxy(-18, (short)i);
+		gotoXY(-16, (short)i);
 		printf("▧");
-		gotoxy(-2, (short)i);
+		gotoXY(0, (short)i);
+		printf("▧");
+	}
+
+	// 오른쪽 현황판 벽 그리기 (가로)
+	for (int i = 0; i < 9; i++)
+	{
+		gotoXY((short)((i + MAX_COL + 1) * 2), 0);
+		printf("▧");
+		gotoXY((short)((i + MAX_COL + 1) * 2), 9);
+		printf("▧");
+		gotoXY((short)((i + MAX_COL + 1) * 2), 21);
+		printf("▧");
+	}
+	// 오른쪽 현황판 벽 그리기 (세로)
+	for (int i = 0; i < 21; i++)
+	{
+		gotoXY((MAX_COL + 1) * 2, (short)i);
+		printf("▧");
+		gotoXY((MAX_COL + 9) * 2, (short)i);
 		printf("▧");
 	}
 
 	setTextColor(SKYBLUE);
-	gotoxy(-15, 15);
+	gotoXY((MAX_COL + 4) * 2 + 1, 2);
+	printf("NEXT");
+	
+	gotoXY(-13, 9);
 	printf("← : 왼쪽");
-	gotoxy(-15, 16);
+	gotoXY(-13, 11);
 	printf("→ : 오른쪽");
-	gotoxy(-15, 17);
+	gotoXY(-13, 13);
 	printf("↑ : 회전");
-	gotoxy(-15, 18);
+	gotoXY(-13, 15);
 	printf("↓ : 밑으로");
-	gotoxy(-15, 19);
+	gotoXY(-13, 17);
 	printf("SPACE : 낙하");
+	gotoXY(-13, 19);
+	printf("Q : 종료");
+}
+
+void updateScore()
+{
+	int score = 0;
+
+	gotoXY((MAX_COL + 3) * 2 - 1, 13);
+	if (lineClearAtOnce > 0)
+	{
+		switch (lineClearAtOnce) {
+		case 1: setTextColor(CYAN);		score = 10; break;
+		case 2: setTextColor(SKYBLUE);	score = 30; break;
+		case 3: setTextColor(GREEN);	score = 60; break;
+		default: setTextColor(RED);		score = 100; break;
+		}
+		printf("%d 줄 클리어", lineClearAtOnce);
+	}
+	else
+	{
+		printf("            ");
+	}
+
+	gotoXY((MAX_COL + 3) * 2, 15);
+	if (currentCombo > 0)
+	{
+		switch (currentCombo) {
+		case 1: setTextColor(GOLD);		score *= 2; break;
+		case 2: setTextColor(YELLOW);	score *= 3; break;
+		case 3: setTextColor(LIGHTRED); score *= 4; break;
+		default: setTextColor(RED);		score *= 5; break;
+		}
+		printf("%d 콤보 !!", currentCombo);
+	}
+	else
+	{
+		printf("          ");
+	}
+
+	setTextColor(LIGHTGREEN);
+	gotoXY((MAX_COL + 4) * 2, 11);
+	printf("          ");
+	if (score > 0)
+	{
+		gotoXY((MAX_COL + 4) * 2, 11);
+		printf("+%d점", score);
+	}
+
+	setTextColor(LIGHTGREEN);
+	gotoXY(-11, 2);
+	printf("Stage 1");
+	
+	totalScore += score;
+	setTextColor(YELLOW);
+	gotoXY(-11, 4);
+	printf("%d 점", totalScore);
+
+	setTextColor(YELLOW);
+	gotoXY((MAX_COL + 3) * 2 - 1, 18);
+	printf("최대콤보 : %d", maxCombo);
 }
 
 void keyProcess()
@@ -459,5 +616,6 @@ void run()
 	{
 		keyProcess();
 		autoDrop();
+		Sleep(1);
 	}
 }
